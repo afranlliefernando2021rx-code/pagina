7173dc1bbe4e86e700f58195e423de584281fe99
Fernando Afranllie <afranlliefernando2021rx@gmail.com>
2026-06-26 00:37:05 -0300

    agregado seccion compra

--- js/script.js
+++ js/script.js
@@ -11,15 +11,87 @@ const imgBrasil = document.getElementById("imgBrasil");
 const imgArgentina = document.getElementById("imgArgentina");
 const volverBrasil = document.getElementById("volverBrasil");
 const volverArgentina = document.getElementById("volverArgentina");
-const logoInicio = document.getElementById("logoInicio");
-const tituloInicio = document.getElementById("tituloInicio");
-
+const logoInicio = document.getElementById("logoInicio")
+const detalleRojoBoca = document.getElementById("detalleRojoBoca");
+const detalleRiver = document.getElementById("detalleRiver");
+const detalleBarcelonaReal = document.getElementById("detalleBarcelonaReal");
+const imgIndependiente = document.getElementById("imgIndependiente");
+const imgRiver = document.getElementById("imgRiver");
+const imgBarcelona = document.getElementById("imgBarcelona");
+const volverRojoBoca = document.getElementById("volverRojoBoca");
+const volverRiver = document.getElementById("volverRiver");
+const volverBarcelonaReal = document.getElementById("volverBarcelonaReal");
 
 
 const btnInicio = document.getElementById("btnInicio");
 const btnSelecciones = document.getElementById("btnSelecciones");
 const btnEquipos = document.getElementById("btnEquipos");
 const btnCompra = document.getElementById("btnCompra");
+const formCompra = document.getElementById("formCompra");
+const resumenCompra = document.getElementById("resumenCompra");
+
+const listaCarrito = document.getElementById("listaCarrito");
+const total = document.getElementById("total");
+
+let carrito = " ";
+let precioTotal = 0;
+const btnItalia = document.getElementById("btnItalia");
+btnItalia.addEventListener("click", () => {
+    carrito += "<li>Italia 1994 - $40.000</li>";
+    listaCarrito.innerHTML = carrito;
+    precioTotal += 40000;
+    total.innerHTML = "Total: $" + precioTotal;
+});
+const btnBrasil = document.getElementById("btnBrasil");
+btnBrasil.addEventListener("click", () => {
+    carrito += "<li>Brasil 2002 - $40.000</li>";
+    listaCarrito.innerHTML = carrito;
+    precioTotal += 40000;
+    total.innerHTML = "Total: $" + precioTotal;
+});
+const btnArgentina = document.getElementById("btnArgentina");
+btnArgentina.addEventListener("click", () => {
+    carrito += "<li>Argentina 2006 - $40.000</li>";
+    listaCarrito.innerHTML = carrito;
+    precioTotal += 40000;
+    total.innerHTML = "Total: $" + precioTotal;
+});
+const btnIndependiente = document.getElementById("btnIndependiente");
+btnIndependiente.addEventListener("click", () => {
+    carrito += "<li>Independiente 1994 - $40.000</li>";
+    listaCarrito.innerHTML = carrito;
+    precioTotal += 40000;
+    total.innerHTML = "Total: $" + precioTotal;
+});
+const btnBoca = document.getElementById("btnBoca");
+btnBoca.addEventListener("click", () => {
+    carrito += "<li>Boca 1994 - $40.000</li>";
+    listaCarrito.innerHTML = carrito;
+    precioTotal += 40000;
+    total.innerHTML = "Total: $" + precioTotal;
+});
+const btnRiver = document.getElementById("btnRiver");
+btnRiver.addEventListener("click", () => {
+    carrito += "<li>River 1999 - $40.000</li>";
+    listaCarrito.innerHTML = carrito;
+    precioTotal += 40000;
+    total.innerHTML = "Total: $" + precioTotal;
+});
+const btnBarcelona = document.getElementById("btnBarcelona");
+btnBarcelona.addEventListener("click", () => {
+    carrito += "<li>Barcelona 2005 - $40.000</li>";
+    listaCarrito.innerHTML = carrito;
+    precioTotal += 40000;
+    total.innerHTML = "Total: $" + precioTotal;
+});
+const btnReal = document.getElementById("btnReal");
+btnReal.addEventListener("click", () => {
+    carrito += "<li>Real Madrid 2005 - $40.000</li>";
+    listaCarrito.innerHTML = carrito;
+    precioTotal += 40000;
+    total.innerHTML = "Total: $" + precioTotal;
+});
+
 
 function ocultarTodo() {
     inicio.classList.add("d-none");
@@ -29,8 +101,12 @@ function ocultarTodo() {
     detalleItalia.classList.add("d-none");
     detalleBrasil.classList.add("d-none");
     detalleArgentina.classList.add("d-none");
+    detalleRojoBoca.classList.add("d-none");
+    detalleRiver.classList.add("d-none");
+    detalleBarcelonaReal.classList.add("d-none");
 
 }
+
 
 
 btnInicio.addEventListener("click", () => {
@@ -77,7 +153,31 @@ logoInicio.addEventListener("click", () => {
     ocultarTodo();
     inicio.classList.remove("d-none");
 });
-tituloInicio.addEventListener("click", () => {
+imgIndependiente.addEventListener("click", () => {
     ocultarTodo();
-    inicio.classList.remove("d-none");
+    detalleRojoBoca.classList.remove("d-none");
+});
+imgRiver.addEventListener("click", () => {
+    ocultarTodo();
+    detalleRiver.classList.remove("d-none");
+});
+imgBarcelona.addEventListener("click", () => {
+    ocultarTodo();
+    detalleBarcelonaReal.classList.remove("d-none");
+});
+volverRojoBoca.addEventListener("click", () => {
+    ocultarTodo();
+    equipos.classList.remove("d-none");
+});
+volverRiver.addEventListener("click", () => {
+    ocultarTodo();
+    equipos.classList.remove("d-none");
+});
+volverBarcelonaReal.addEventListener("click", () => {
+    ocultarTodo();
+    equipos.classList.remove("d-none");
+});
+formCompra.addEventListener("submit", function(e){
+    e.preventDefault();
+    resumenCompra.innerHTML = "<h3>Compra realizada con éxito</h3>" + "<p><strong>Total:</strong> $" + precioTotal + "</p>" + "Gracias por su compra</p>";
 });
